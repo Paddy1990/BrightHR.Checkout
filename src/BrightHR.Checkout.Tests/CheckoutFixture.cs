1ab7701d5899d514f520c17d42d00463b920a194
Paddy Halle <paddy.halle@outlook.com>
2021-02-07 15:11:17 +0000

    Added test data for Products and Offers in CheckoutFixture class

--- src/BrightHR.Checkout.Tests/CheckoutFixture.cs
+++ src/BrightHR.Checkout.Tests/CheckoutFixture.cs
@@ -1,13 +1,31 @@
+using BrightHR.Checkout.Models;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using Xunit;
 
 namespace BrightHR.Checkout.Tests
 {
     public class CheckoutFixture
     {
+        private IEnumerable<Product> _products;
+        private IEnumerable<Offer> _offers;
+
         public CheckoutFixture()
         {
+            _products = new List<Product>
+            {
+                new Product("A", 50m),
+                new Product("B", 30m),
+                new Product("C", 20m),
+                new Product("D", 15m),
+            };
 
+            _offers = new List<Offer>
+            {
+                new Offer("A", 3, 130m),
+                new Offer("B", 2, 45m),
+            };
         }
     }
 }
